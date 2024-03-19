c2897fa210fed562e257b1f15122f0a5a728732f
Usuario <Usuario@CPD03-M08>
2024-03-19 11:07:26 -0300

    Adicionar arquivos de projeto.

---
+++ Operacoes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite qual operação deseja utilizar (/, *, -, +): ");
+            string operacao = Console.ReadLine();
+
+            switch (operacao)
+            {
+                case "/":
+                    double divisao = n1 / n2;
+                    Console.WriteLine("O resultado da operação é: " + divisao);
+                    break;
+                case "*":
+                    double mult = n1 * n2;
+                    Console.WriteLine("O resultado da operação é: " + mult);
+                    break;
+                case "-":
+                    double subtracao = n1 - n2;
+                    Console.WriteLine("O resultado da operação é: " + subtracao);
+                    break;
+                case "+":
+                    double soma = n1 + n2;
+                    Console.WriteLine("O resultado da operação é: " +soma);
+                    break;
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
